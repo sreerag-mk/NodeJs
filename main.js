b991d18f75127531ee4f3f063bc0963a56cd1153
sreerag <sreeragmanoj07.com>
2023-10-11 18:02:33 +0530

    edit

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const http = require("node:http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200);
-  res.end("seerag manoj");
+  res.end("seerag manoj this isnew new n ewen wne nw e");
 });
 
 server.listen(3000, () => {
-  console.log("Server running on post 3000");
+  console.log("Sreerag manoj");
 });
